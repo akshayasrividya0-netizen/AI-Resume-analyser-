007d17e3e0c5d62e97b0ebde5e0c8d6d219efdf9
akshayasrividya0-netizen <akshayasrividya0@gmail.com>
2026-07-07 21:56:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import streamlit as st
 
+st.title("AI Resume Analyzer")
+
+st.write("Welcome to AI Resume Analyzer")
+
+resume = st.file_uploader("Upload your Resume (PDF)", type=["pdf"])
+
+job = st.text_area("Paste Job Description")
+
+if st.button("Analyze"):
+    if resume and job:
+        st.success("Resume uploaded successfully!")
+        st.write("Job Description:")
+        st.write(job)
+    else:
+        st.warning("Please upload a resume and enter a job description.")
